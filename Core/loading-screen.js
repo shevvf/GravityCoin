0a7002d461d1918b70756810d3041d551260e589
shevvf <kshevlev@mail.ru>
2024-07-06 20:33:28 +0300

    Initial Commit

---
+++ Core/loading-screen.js
@@ -0,0 +1,73 @@
+﻿// window.addEventListener("load", function () {
+//     if ("serviceWorker" in navigator) {
+//       navigator.serviceWorker.register("ServiceWorker.js");
+//     }
+//   });
+  var unityInstanceRef;
+  var unsubscribe;
+  const container = document.querySelector("#unity-container");
+  const canvas = document.querySelector("#unity-canvas");
+  const loadingRoot = document.getElementById("loading-screen");
+  const progressBar = document.getElementById("progress-bar");
+  const progressText = document.getElementById("progress-text");
+
+  var buildUrl = "Build";
+  var loaderUrl = buildUrl + "/GravityCoin.loader.js";
+  var config = {
+    dataUrl: buildUrl + "/GravityCoin.data.unityweb",
+    frameworkUrl: buildUrl + "/GravityCoin.framework.js.unityweb",
+    codeUrl: buildUrl + "/GravityCoin.wasm.unityweb",
+    streamingAssetsUrl: "StreamingAssets",
+    companyName: "DefaultCompany",
+    productName: "AIVIKTestTask",
+    productVersion: "0.1",
+  };
+
+  if (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent)) {
+      container.className = "unity-mobile";
+  }
+
+  function focusOnGame() {
+      container.focus();
+      window.focus();
+      canvas.focus();
+  }
+
+  function updateProgress(progress) {
+      var int_progress = Math.ceil(progress / 0.9);
+      int_progress = int_progress > 100 ?
+          100 : int_progress < 0 ? 0 : int_progress;
+      progressBar.style.width = `${int_progress}%`;
+      progressText.textContent = `${int_progress}%`;
+  }
+
+  let loadingDone = false;
+
+  function hideLoadingScreen() {
+      if (loadingDone) return;
+      document.getElementById("loading-screen").style.opacity = 0.0;
+      setTimeout(() => {
+          loadingRoot.style.display = "none";
+          loadingRoot.remove();
+          console.log("Loading screen is done and deleted.");
+      }, 500);
+      loadingDone = true;
+  }
+
+  const script = document.createElement("script");
+  script.src = loaderUrl;
+  script.onload = () => {
+      createUnityInstance(canvas, config, (progress) => {
+          updateProgress(100 * progress);
+      }).then((unityInstance) => {
+          unityInstanceRef = unityInstance;
+          loadProgress();
+          hideLoadingScreen();
+          focusOnGame();
+      }).catch((message) => {
+          alert(message);
+      });
+};
+
+document.body.appendChild(script);
+document.addEventListener("pointerdown", focusOnGame);
---
+++ Core/storage-service.js
@@ -0,0 +1,76 @@
+﻿function loadProgress(){
+    getAllKeys(function(keys) {
+        if (keys && keys.length > 0) {
+            getValues(keys, function(values) {
+                console.log('Retrieved keys and values:', values);
+                unityInstanceRef.SendMessage('VCRootScope', 'TestData')
+            });
+        } else {
+            console.log('No keys found in storage');
+        }
+    });
+}
+
+function saveProgress(key, value){
+    setValue(key, value);
+}
+
+function setValue(key, value) {
+    window.Telegram.WebApp.setItem(key, value, function(error, success) {
+        if (error) {
+            console.error('Error storing the value:', error);
+        } else if (success) {
+            console.log('Value successfully stored');
+        }
+    });
+}
+
+function getValue(key) {
+    window.Telegram.WebApp.getItem(key, function(error, value) {
+        if (error) {
+            console.error('Error getting the value:', error);
+        } else {
+            console.log('Retrieved value:', value);
+        }
+    });
+}
+
+function getValues(keys) {
+    window.Telegram.WebApp.getItems(keys, function(error, values) {
+        if (error) {
+            console.error('Error getting the values:', error);
+        } else {
+            console.log('Retrieved values:', values);
+        }
+    });
+}
+
+function removeValue(key) {
+    window.Telegram.WebApp.removeItem(key, function(error, success) {
+        if (error) {
+            console.error('Error removing the value:', error);
+        } else if (success) {
+            console.log('Value successfully removed');
+        }
+    });
+}
+
+function removeValues(keys) {
+    window.Telegram.WebApp.removeItems(keys, function(error, success) {
+        if (error) {
+            console.error('Error removing the values:', error);
+        } else if (success) {
+            console.log('Values successfully removed');
+        }
+    });
+}
+
+function getAllKeys() {
+    window.Telegram.WebApp.getKeys(function(error, keys) {
+        if (error) {
+            console.error('Error getting the keys:', error);
+        } else {
+            console.log('Retrieved keys:', keys);
+        }
+    });
+}
