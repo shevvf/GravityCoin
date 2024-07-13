3b6f43e693b54d1ce1a113382a526f098e9c8bb4
shevvf <kshevlev@mail.ru>
2024-07-13 11:51:48 +0300

    backup

--- Core/storage-service.js
+++ Core/storage-service.js
@@ -1,5 +1,4 @@
-﻿﻿// Преобразование getAllKeys в функцию, возвращающую Promise
-function getAllKeys() {
+﻿﻿function getAllKeys() {
     return new Promise((resolve, reject) => {
         window.Telegram.WebApp.CloudStorage.getKeys((error, keys) => {
             if (error) {
@@ -11,41 +10,22 @@ function getAllKeys() {
     });
 }
 
-// Преобразование getValues в функцию, возвращающую Promise
 function getValues(keys) {
-    const promises = keys.map(key => getValue(key));
-    return Promise.all(promises)
-        .then(values => {
-            const result = {};
-            keys.forEach((key, index) => {
-                result[key] = values[index];
-            });
-            return result;
-        });
-}
-
-// Функция для получения значения из хранилища
-function getValue(key) {
     return new Promise((resolve, reject) => {
-        window.Telegram.WebApp.CloudStorage.getItem(key, (error, value) => {
+        window.Telegram.WebApp.CloudStorage.getItems(keys, (error, values) => {
             if (error) {
-                reject('Error getting the value: ' + error);
+                reject('Error getting the values: ' + error);
             } else {
-                try {
-                    resolve(JSON.parse(value)); // Парсим значение как JSON
-                } catch (e) {
-                    resolve(value); // Если это не JSON, возвращаем как есть
-                }
+                resolve(values);
             }
         });
     });
 }
 
-// Функция для загрузки прогресса с использованием Promises
 function loadProgress() {
     return getAllKeys()
         .then(keys => {
-            console.log('Retrieved keys:', keys); // Дополнительная отладочная информация
+            console.log('Retrieved keys:', keys);
             if (keys && keys.length > 0) {
                 return getValues(keys);
             } else {
@@ -55,7 +35,7 @@ function loadProgress() {
         })
         .then(values => {
             if (values) {
-                console.log('Retrieved keys and values:', values); // Дополнительная отладочная информация
+                console.log('Retrieved keys and values:', values);
                 return values;
             } else {
                 return null;
@@ -67,19 +47,13 @@ function loadProgress() {
         });
 }
 
-// Функция для сохранения прогресса
-function saveProgress(key, value) {
+function saveProgress(key, jsonValue){
+    var value = JSON.parse(jsonValue);
     setValue(key, value);
 }
 
-// Функция для установки значения в хранилище
 function setValue(key, value) {
-    const params = {
-        key: key,
-        value: value
-    };
-    
-    window.Telegram.WebApp.CloudStorage.saveStorageValue(params, function(error, success) {
+    window.Telegram.WebApp.CloudStorage.setItem(key, value, function(error, success) {
         if (error) {
             console.error('Error storing the value:', error);
         } else if (success) {
@@ -88,7 +62,16 @@ function setValue(key, value) {
     });
 }
 
-// Функция для удаления значения из хранилища
+function getValue(key) {
+    window.Telegram.WebApp.CloudStorage.getItem(key, function(error, value) {
+        if (error) {
+            console.error('Error getting the value:', error);
+        } else {
+            console.log('Retrieved value:', value);
+        }
+    });
+}
+
 function removeValue(key) {
     window.Telegram.WebApp.CloudStorage.removeItem(key, function(error, success) {
         if (error) {
@@ -99,7 +82,6 @@ function removeValue(key) {
     });
 }
 
-// Функция для удаления нескольких значений из хранилища
 function removeValues(keys) {
     window.Telegram.WebApp.CloudStorage.removeItems(keys, function(error, success) {
         if (error) {
